bc9776da73142b7885725857c8ae6f3aa74c282a
Felix Wang <37761392+SuperTiger-ops@users.noreply.github.com>
2020-09-13 09:29:04 +0800

    Update Migration-DreamADGroupMemebers.ps1

--- Migration-DreamADGroupMemebers.ps1
+++ Migration-DreamADGroupMemebers.ps1
@@ -8,21 +8,27 @@ Function Migration-DreamADGroupMemebers{
        [string]$sourcegroupname     
     )
 
-    $desmembers = Get-ADGroupMember -Identity $desgroupname
-    Write-Host $desmembers.count
+    if($desgroupname -and $sourcegroupname){
 
-    $sourcemembers = Get-ADGroupMember -Identity $sourcegroupname
-    Write-Host $sourcemembers.count
+        $desmembers = Get-ADGroup -Identity $desgroupname -Properties members | Select-Object  -ExpandProperty members
+        Write-Host Desgroup start numbers count is $desmembers.count
 
-    foreach($item in $sourcemembers){
-        try{
-            Add-ADGroupMember -Identity $desgroupname -Members $item.SamAccountName
+        $sourcemembers = Get-ADGroup -Identity $sourcegroupname -Properties members | Select-Object -ExpandProperty members
+        Write-Host Sourcegroup numbers count is $sourcemembers.count
+
+        foreach($item in $sourcemembers){
+            try{
+                Write-Host Start migration $item
+                Add-ADGroupMember -Identity $desgroupname -Members $item
+            }
+            catch{
+                Write-Host $Error[0]
+            }
         }
-        catch{
-            Write-Host $Error[0]
-        }
+
+        $desEndmembers = Get-ADGroup -Identity $desgroupname -Properties members | Select-Object -ExpandProperty members
+        Write-Host Desgroup End numbers count is $desEndmembers.count
+    }else{
+        Write-Host "Please input a valid group name."
     }
-
-    $desEndmembers = Get-ADGroupMember -Identity $desgroupname
-    Write-Host $desEndmembers.count
 }
